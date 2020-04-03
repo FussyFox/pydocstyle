b0b65dcf3a9ff5917226df8b6d6dbf1145282a75
Johannes Hoppe <info@johanneshoppe.com>
2020-04-03 16:08:20 +0200

    Remove explicit logger setup

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 """Lambda function that executes pydocstyle, a static file linter."""
-import logging
-import sys
 
 from lintipy import CheckRun
-
-root_logger = logging.getLogger('')
-root_logger.setLevel(logging.DEBUG)
-root_logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
 handle = CheckRun.as_handler('pydocstyle', 'pydocstyle', '.')
